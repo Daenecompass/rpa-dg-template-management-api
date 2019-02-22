b00d2396ef6c36272fcc023715f91fbb5a70a66c
Linus Norton <linusnorton@gmail.com>
2019-02-22 16:02:32 +0000

    Get files in JAR

--- src/main/java/uk/gov/hmcts/reform/dg/template/management/repository/LocalTemplateListRepository.java
+++ src/main/java/uk/gov/hmcts/reform/dg/template/management/repository/LocalTemplateListRepository.java
@@ -1,37 +1,29 @@
 package uk.gov.hmcts.reform.dg.template.management.repository;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static pl.touk.throwing.ThrowingFunction.unchecked;
 
 public class LocalTemplateListRepository implements TemplateListRepository {
 
     @Override
     public List<Template> getTemplates() throws IOException {
-        try (Stream<Path> files = Files.walk(getPath())) {
-            return files.filter(Files::isRegularFile)
-                .sorted()
-                .map(f -> new Template(f.getFileName().toString()))
-                .collect(Collectors.toList());
-        }
-    }
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources("classpath:templates/*.*");
 
-    private Path getPath() throws IOException {
-        try {
-            URL templateDirectory = Thread.currentThread().getContextClassLoader().getResource("templates" + File.separator);
-            File templateFile = new File(templateDirectory.toURI());
-
-            return Paths.get(templateFile.getAbsolutePath());
-        } catch (URISyntaxException e) {
-            throw new IOException(e);
-        }
+        return Arrays.stream(resources)
+            .map(unchecked(Resource::getFile))
+            .filter(File::isFile)
+            .sorted()
+            .map(f -> new Template(f.getName()))
+            .collect(Collectors.toList());
     }
 
 }
