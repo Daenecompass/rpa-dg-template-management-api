93393102efdabcb3cbc0084a146a073d21736b8d
Linus Norton <linusnorton@gmail.com>
2019-02-25 10:48:55 +0000

    Add saat and sprod tfvars

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -115,7 +115,7 @@ variable "enable_s2s_healthcheck" {
 // Whitelists
 ////////////////////////////////////////////////
 variable "s2s_names_whitelist" {
-  default = "em_api,em_gw,ccd_gw,ccd_data,sscs,divorce_document_upload,divorce_document_generator,probate_backend,jui_webapp,pui_webapp"
+  default = "em_api,em_gw,jui_webapp,pui_webapp,dg_docassembly"
 }
 
 variable "case_worker_roles" {
