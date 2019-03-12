68d14e9e22506c37925fc549f81bd3d8320e201a
Linus Norton <linusnorton@gmail.com>
2019-03-12 22:52:18 +0000

    Set the whitelist

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -8,7 +8,7 @@ output "s2s_url" {
 }
 
 output "idam_webshow_whitelist" {
-  value = ""https://em-show-aat-staging.service.core-compute-aat.internal/oauth2/callback""
+  value = "https://em-show-aat-staging.service.core-compute-aat.internal/oauth2/callback"
 }
 
 output "enable_idam_health_check" {
