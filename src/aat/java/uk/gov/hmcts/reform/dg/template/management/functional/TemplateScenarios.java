8e9aba2d6b9c64d53d35bf27a9867e8b106e436f
Linus Norton <linusnorton@gmail.com>
2019-03-20 13:43:29 +0000

    Remove idam client (#15)

--- src/aat/java/uk/gov/hmcts/reform/dg/template/management/functional/TemplateScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/dg/template/management/functional/TemplateScenarios.java
@@ -1,53 +1,22 @@
 package uk.gov.hmcts.reform.dg.template.management.functional;
 
-import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.dg.template.management.Application;
 import uk.gov.hmcts.reform.dg.template.management.testutil.Env;
-import uk.gov.hmcts.reform.dg.template.management.testutil.IdamHelper;
-import uk.gov.hmcts.reform.dg.template.management.testutil.S2sHelper;
-
+import uk.gov.hmcts.reform.dg.template.management.testutil.TestUtil;
 import java.util.Base64;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, properties = "SpringBootTest")
-@TestPropertySource(locations = "classpath:application-aat.yaml")
-@ActiveProfiles("aat")
 public class TemplateScenarios {
 
-    @Autowired
-    private IdamHelper idamHelper;
-
-    @Autowired
-    private S2sHelper s2sHelper;
-
-    private String idamAuth;
-    private String s2sAuth;
-
-    @Before
-    public void setup() {
-        idamAuth = idamHelper.getIdamToken();
-        s2sAuth = s2sHelper.getS2sToken();
-
-        RestAssured.useRelaxedHTTPSValidation();
-    }
+    TestUtil testUtil = new TestUtil();
 
     @Test
     public void testGetTemplates() {
-        Response response = RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        Response response = testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
             .request("GET", Env.getTestUrl() + "/api/templates");
 
@@ -67,9 +36,8 @@ public class TemplateScenarios {
     public void testGetTemplate() {
         String id = Base64.getEncoder().encodeToString("TB-IAC-APP-ENG-00003 Template Tornado.docx".getBytes());
 
-        Response response = RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        Response response = testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
             .request("GET", Env.getTestUrl() + "/api/templates/" + id);
 
@@ -83,9 +51,8 @@ public class TemplateScenarios {
     public void testGetTemplate404() {
         String id = Base64.getEncoder().encodeToString("does not exist.docx".getBytes());
 
-        Response response = RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        Response response = testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
             .request("GET", Env.getTestUrl() + "/api/templates/" + id);
 
