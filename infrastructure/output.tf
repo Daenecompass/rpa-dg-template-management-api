6f7f3944333a6ef2ffe7a630e95ae751cdcdc3b4
Linus Norton <linusnorton@gmail.com>
2019-03-25 11:27:39 +0000

    Hell-m

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,16 +1,4 @@
 
-output "idam_api_base_uri" {
-  value = "${var.idam_api_url}"
-}
-
-output "s2s_base_uri" {
-  value = "http://${var.s2s_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
-}
-
-output "idam_webshow_whitelist" {
-  value = "https://em-show-aat.service.core-compute-aat.internal/oauth2/callback"
-}
-
 output "enable_idam_healthcheck" {
   value = "${var.enable_idam_healthcheck}"
 }
